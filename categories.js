f646849e564ba1fbcb3b1a35a4e7317008907b66
clarm3126 <clarm3126@gmail.com>
2026-02-25 12:34:44 +0000

    Fix: Update missing category images

--- categories.js
+++ categories.js
@@ -1,11 +1,11 @@
 export const categories = [
-    { id: 'fashion-accessories', name: '패션/잡화', imageUrl: 'https://images.unsplash.com/photo-1553062407-98eeb68a6a62?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
-    { id: 'digital-appliances', name: '디지털/가전', imageUrl: 'https://images.unsplash.com/photo-1550009158-94ae76552485?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
-    { id: 'food', name: '식품', imageUrl: 'https://images.unsplash.com/photo-1627435601361-ec25f2b740ba?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
+    { id: 'fashion-accessories', name: '패션/잡화', imageUrl: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
+    { id: 'digital-appliances', name: '디지털/가전', imageUrl: 'https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
+    { id: 'food', name: '식품', imageUrl: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
     { id: 'cosmetics-beauty', name: '화장품/미용', imageUrl: 'https://images.unsplash.com/photo-1596462502278-27bfdc403348?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
     { id: 'furniture-interior', name: '가구/인테리어', imageUrl: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
     { id: 'sports-leisure', name: '스포츠/레저', imageUrl: 'https://images.unsplash.com/photo-1517649763962-0c623066013b?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
     { id: 'leisure-convenience', name: '여가/생활편의', imageUrl: 'https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
     { id: 'life-health', name: '생활/건강', imageUrl: 'https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' },
-    { id: 'childbirth-parenting', name: '출산/육아', imageUrl: 'https://images.unsplash.com/photo-1519689689268-1a99a77e966e?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' }
-]; // Forcing a change to clear cache.
+    { id: 'childbirth-parenting', name: '출산/육아', imageUrl: 'https://images.unsplash.com/photo-1525999147711-835474620964?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=600' }
+];
